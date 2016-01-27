0e2223b95c4085047818ad0e26fd161c7128b786
Shanika <sjbutts@gmail.com>
2016-01-27 18:21:54 -0500

    Use SSL and the Puma web server in Production

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 	def setup
-		@user = User.new(name: "Example User", email: "user@example.com")
-							password: "foobar", password_confirmation: "foobar"
+		@user = User.new(name: "Example User", email: "user@example.com",
+							password: "foobar", password_confirmation: "foobar")
 	end
 
 	test "should be valid" do
@@ -41,7 +41,7 @@ class UserTest < ActiveSupport::TestCase
 		valid_addresses.each do |valid_address|
 			@user.email = valid_address
 			assert @user.valid?, "#{valid_address.inspect} should be valid"
-		end
+	end
 
 	test "email addresses should be unique" do
 		duplicate_user = @user.dup
