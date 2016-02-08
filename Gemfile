d4b008b69a0e2fe3287424dd8a202ea4d9239de0
Shanika <sjbutts@gmail.com>
2016-02-08 11:56:03 -0500

    Use SSL and the Puma web server in production

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'faker', '1.4.2'
 gem 'will_paginate', '3.0.7'
 gem 'bootstrap-will_paginate', '0.0.10'
 gem 'bootstrap-sass', '3.2.0.0'
+gem 'puma'
 # Use sqlite3 as the database for Active Record
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
@@ -53,7 +54,6 @@ end
 group :production do
 	gem 'pg', '0.17.1'
 	gem 'rails_12factor', '0.0.2'
-	gem 'puma', '2.11.1'
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token
-	before_save {self.email = email.downcase}
+	attr_accessor :remember_token, :activation_token
+	before_save  :downcase_email
+	before_create :create_activation_digest
 	validates :name, presence: true, length: { maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum: 255}, 
@@ -32,3 +33,22 @@ class User < ActiveRecord::Base
 		update_attribute(:remember_digest, nil)
 	end
 end
+
+private
+	
+
+	#Remembers a user in the database for use in persistent sessions.
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+	#Converts email to all lower-case.
+	def downcase_email
+		self.email = email.downcase
+	end
+
+	#Creates and assigns the activation token and digest.
+	def create_activation_digest
+		self.activation_token = User.new_token
+		self.activation_digest = User.digest(activation_token)
+	end
